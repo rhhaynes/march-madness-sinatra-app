57f1cea538a051c3b376f53dd34930a88be8aec9
R. Harris Haynes <harrishaynes@gmail.com>
2018-04-05 22:26:29 -0400

    added attributes to games table

--- db/migrate/20180406021319_create_games.rb
+++ db/migrate/20180406021319_create_games.rb
@@ -1,4 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.1]
   def change
+    create_table :games do |t|
+      t.string  :round
+      t.integer :bracket_id
+      t.integer :team_id_winner
+      t.integer :team_id_loser
+    end
   end
 end
