4ce5367f30f9f6b02daa3013e2e223c4457c7df4
R. Harris Haynes <harrishaynes@gmail.com>
2018-04-05 22:26:47 -0400

    added attributes to teams table

--- db/migrate/20180406021416_create_teams.rb
+++ db/migrate/20180406021416_create_teams.rb
@@ -1,4 +1,12 @@
 class CreateTeams < ActiveRecord::Migration[5.1]
   def change
+    create_table :teams do |t|
+      t.string  :name
+      t.string  :name_abbreviation
+      t.string  :nickname
+      t.string  :season_record
+      t.string  :region
+      t.integer :region_seed
+    end
   end
 end
