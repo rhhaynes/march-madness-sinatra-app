186590b6e22e18e2760a2274949336c3f8d03f7f
R. Harris Haynes <harrishaynes@gmail.com>
2018-04-05 22:25:56 -0400

    added attributes to brackets table

--- db/migrate/20180406021215_create_brackets.rb
+++ db/migrate/20180406021215_create_brackets.rb
@@ -1,4 +1,10 @@
 class CreateBrackets < ActiveRecord::Migration[5.1]
   def change
+    create_table :brackets do |t|
+      t.string    :name
+      t.integer   :user_id
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
   end
 end
